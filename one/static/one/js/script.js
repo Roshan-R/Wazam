e72a2605d539b5004bcb055de5d0728f0cecba3f
GameGodS3 <sudevssuresh@gmail.com>
2021-02-06 23:13:40 +0530

    card reveal

--- one/static/one/js/script.js
+++ one/static/one/js/script.js
@@ -58,7 +58,10 @@ function gotSong(response) {
         console.log("Track coverart : " + response.track.images.coverart)
 
         const trackData = getTrackData(response);
-        document.body.insertAdjacentHTML("afterend", trackData);
+        document.querySelector(".wrapper").insertAdjacentHTML("beforeend", trackData);
+        document.querySelector(".card-item").classList.add("cardreveal");
+        document.querySelector(".recordButton").classList.add("rollaway");
+        document.querySelector(".card-info").scrollIntoView();
 
     } else {
         console.log("No match found");
@@ -80,6 +83,7 @@ function JSmagic() {
 
     }
 }
+document.querySelector(".recordButton").addEventListener("click", JSmagic);
 
 function getTrackData(response) {
     return `
@@ -87,7 +91,7 @@ function getTrackData(response) {
     <div class="cards">
         <div class="card-item">
             <div class="card-image">
-                <img class="img-responsive" src="${response.track.images.coverart}">"
+                <img class="img-responsive" src="${response.track.images.coverart}">
             </div>
             <div class="card-info">
                 <div class="card-title">
